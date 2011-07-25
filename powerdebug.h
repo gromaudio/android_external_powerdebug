856efcd3eb3b2ac0c0742aa115054cdffb4c4bf2
Daniel Lezcano <daniel.lezcano@linaro.org>
2011-07-25 23:08:54 +0200

    Version 0.5.1
     * Fixed segfault when no clock is available.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- powerdebug.h
+++ powerdebug.h
@@ -13,6 +13,4 @@
  *       - initial API and implementation
  *******************************************************************************/
 
-#define VERSION "0.5.0"
-
-
+#define VERSION "0.5.1"
